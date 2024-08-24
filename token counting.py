7bb50fb81c57f5807ebe4ee85b20e27aca42b2c0
Tom <tomlockwood135@outlook.com>
2024-08-24 21:48:26 +0100

    testing

--- token counting.py
+++ token counting.py
@@ -3,9 +3,10 @@ from tools import Tools
 
 # print (Tools.get_news(country="gb"))
 
-print(Tools.get_directions("London", "Portsmouth", include_steps=True))
+
+# print(Tools.take_notes(notes="I need to buy some groceries"))
+
+# print(Tools.take_notes(search=True, query="24-08-2024"))
 
 
-
-
-
+# print(Tools.send_user_message(user_id=, message=""))
