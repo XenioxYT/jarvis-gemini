4ae5596a1aa9453f7576060a8c443ae0751a0a16
Tom <tomlockwood135@outlook.com>
2024-09-04 23:07:41 +0100

    testing for tools refactor

--- token counting.py
+++ token counting.py
@@ -1,12 +1,16 @@
 # Testing tools
 from tools import Tools
 
-# print (Tools.get_news(country="gb"))
+print (Tools.get_news(headlines_only=True))
 
+print(Tools.get_place_information("The five swans"))
 
-# print(Tools.take_notes(notes="I need to buy some groceries"))
+print(Tools.get_weather("Newcastle upon tyne"))
 
-# print(Tools.take_notes(search=True, query="24-08-2024"))
+print(Tools.get_directions("Newcastle upon tyne", "London"))
 
+print(Tools.take_notes(notes = "I am testing the notes tool"))
 
-# print(Tools.send_user_message(user_id=, message=""))
+print(Tools.google_search("I am testing the google search tool"))
+
+print(Tools.send_message_to_phone("1234", "I am testing the send message to phone tool"))
